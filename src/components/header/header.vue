c155378d8be1efd7e84c6cbe3837d6922467d563
wq199909 <2749374330@qq.com>
2019-09-30 08:46:48 +0800

    default

---
+++ src/components/header/header.vue
@@ -0,0 +1,33 @@
+<template>
+    <el-row class="header">
+        <el-col :span="4">
+            <router-link to='/' class="logo">
+                <img src="@/assets/image/logo.jpg" alt="">
+            </router-link>
+        </el-col>
+        <el-col :span="10">
+            <div class="title">
+                <span>选</span>
+                <span>课</span>
+                <span>系</span>
+                <span>统</span>
+            </div>
+        </el-col>
+        <el-col :span="6">
+
+        </el-col>
+        <el-col :span="4">
+            <router-link to='/login' class='login'>登录</router-link>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/layout/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-container>
+    <el-header>
+        <my-header />
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-main>Main</el-main>
+        <el-footer>@汪清 洪紫阳 李家成</el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import myHeader from '@/components/header/header'
+export default {
+    components:{
+        myHeader
+    }
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/css/public/header.scss"
+</style>
