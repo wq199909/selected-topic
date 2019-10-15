8734914847f338689f247f4c87ad7b22c72205d9
wq199909 <2749374330@qq.com>
2019-10-15 13:35:47 +0800

    创建小组

--- src/components/group/createGroup.vue
+++ src/components/group/createGroup.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="create">
+  <div class="createGroup">
     <div v-if="hasTeam">你已在队伍中</div>
     <div v-else>
       <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="活动名称">
-          <el-input v-model="form.name"></el-input>
+        <el-form-item label="队伍名称">
+          <el-input v-model="form.name" key="teamName"></el-input>
         </el-form-item>
-        <el-form-item label="即时配送">
+        <el-form-item label="队伍加密">
           <el-switch v-model="form.delivery"></el-switch>
+          <span class="el-icon-warning-outline">加密时他人加入必须输入密码</span>
+        </el-form-item>
+        <el-form-item v-if="form.delivery" label="队伍密码">
+          <el-input v-model="form.passWord"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="onSubmit">立即创建</el-button>
-          <el-button>取消</el-button>
+          <el-button @click="reset">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -19,19 +23,49 @@
 </template>
 
 <script>
+import api from "@/api/index.js";
 export default {
     data(){
         return{
             form:{
                 name:'',
-                delivery: fasle,
-                
+                delivery: false,
+                passWord:''
             }
         }
     },
   computed: {
     hasTeam: function() {
       return this.$store.state.user.teamId != null;
+    }
+  },
+  methods:{
+    onSubmit(){
+      if(this.form.name==''){
+        this.$message('队伍不能为空！')
+      }else if(this.form.delivery && this.form.passWord){
+        this.$message('开启加密时密码不能为空')
+      }else{
+        api.createTeam({
+          teamName: this.form.name,
+          passWord: this.form.passWord,
+          encryption: this.form.delivery,
+          class: this.$store.state.user.class,
+          userName: this.$store.state.user.userName,
+          userId: this.$store.state.user.userId
+        }).then((res)=>{
+          if(res.data.status){
+            this.$message('队伍创建成功');
+            this.reset();
+            this.$store.state.user.teamId = res.data.data.teamId;
+          }
+        })
+      }
+    },
+    reset(){
+      this.form.name = '';
+      this.form.delivery = false;
+      this.form.passWord = '';
     }
   }
 };
--- src/components/group/myGroup.vue
+++ src/components/group/myGroup.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="myGroup">
-    <el-card class="box-card">
+    <el-card class="member">
       <div slot="header" class="clearfix">
-        <span>卡片名称</span>
+        <span>成员</span>
         <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
       </div>
-      <div v-for="o in 4" :key="o" class="text item">{{'列表内容 ' + o }}</div>
+      <div v-for="o in teamMembers" :key="o" class="text item">{{'列表内容 ' + o }}</div>
+    </el-card>
+    <el-card class="topic">
+      <div slot="header" class="clearfix">
+        <span>选题</span>
+        <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+      </div>
+      <div v-for="o in topicList" :key="o" class="text item">{{'列表内容 ' + o }}</div>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+    return {
+      topicList: [],
+      teamMembers: []
+    }
+  }
+};
 </script>
 
 <style>
